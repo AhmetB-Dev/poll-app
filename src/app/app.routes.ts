a6421c76b6c91bcfb3d60e590b7530134616b2ef
AhmetB-Dev <ahmet_ba@web.de>
2026-06-26 13:39:21 +0200

    feat: add routing for home, create survey, survey detail, and not found pages
    refactor: simplify app component by removing unused signal
    
    test: add unit tests for SupabaseService, SurveyService, and VoteService
    
    feat: implement CreateSurvey, Home, NotFound, and SurveyDetail components with basic templates
    
    chore: create model interfaces for Answer, Question, Survey, and Vote
    
    fix: update tsconfig to include necessary files for compilation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { CreateSurvey } from './pages/create-survey/create-survey';
+import { SurveyDetail } from './pages/survey-detail/survey-detail';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'PollApp',
+  },
+  {
+    path: 'create',
+    component: CreateSurvey,
+    title: 'Create Poll',
+  },
+  {
+    path: 'survey/:id',
+    component: SurveyDetail,
+    title: 'Poll Deatails',
+  },
+  {
+    path: '**',
+    component: NotFound,
+    title: 'page not found',
+  },
+];
